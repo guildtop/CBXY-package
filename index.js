2522affde9b6dc99a2e423f0f2840f09d5b8037d
guildtop <81925644+guildtop@users.noreply.github.com>
2021-04-06 12:14:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ function sum(a, b) {
     console.log(a + b);
 }
 
+function commands()  {
+    console.log("Commands soon!")
+}
+
+function news()  {
+    console.log("No News ):")
+}
+
+
 module.exports = { helloworld};
+module.exports = { commands};
+module.exports = { news};
